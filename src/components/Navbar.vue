90ac23e6bfbfdc8642f4477784d7e128ee1f9c16
ashleyrevlett <ashleyrevlett@gmail.com>
2017-05-16 23:21:07 -0400

    Fixes navbar animation

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,11 @@
 <template>
-  <div class="navbar">
-    <ul>
-      <li v-for="link in links" v-on:mouseover="mouseover" v-on:mouseout="mouseout" v-bind:class="{ active: link.active }">
+  <div class="navbar" v-on:mouseover="active=true" v-on:mouseout="endNav">
+    <ul id="navlist" ref="navlist">
+      <li v-for="link in links" v-on:mouseover="mouseover" v-bind:class="{ active: link.active }">
         <a :href="link.url">{{link.title}}</a>
       </li>
     </ul>
-    <transition
-      v-on:before-enter="beforeEnter"
-      v-on:enter="enter"
-      v-on:after-enter="afterEnter"
-      v-on:enter-cancelled="enterCancelled"
-      v-on:before-leave="beforeLeave"
-      v-on:leave="leave"
-      v-on:after-leave="afterLeave"
-      v-on:leave-cancelled="leaveCancelled"
-    >
+    <transition v-on:enter="enter">
       <span id="bar" ref="bar" v-show="active"></span>
     </transition>
   </div>
@@ -35,86 +26,36 @@ export default {
         { title:'company', url:'http://google.com', active: false },
         { title:'contact', url:'http://google.com', active: false }
       ],
-      barLeft: 10,
+      barLeft: 0,
+      animQueue: "custom"
     }
   },
   methods: {
+    endNav: function(e) {
+      // turn off bar, reset inactive state for all links
+      this.active=false;
+      for (var i = 0; i < this.links.length; i++) {
+        this.links[i].active = false;
+      };
+    },
     mouseover: function(e) {
-      // console.log(this);
-      // document.querySelectorAll('li:hover').class = "active";
-      // console.log(document.querySelectorAll('li:hover'));
-      // console.log(this.$el);
-      // this.$el.class = "active";
-      // this.active = true;
-      // e.target.active = true;
-      // console.log(e.target.innerHTML);
-      this.active = true;
+      // find which link in the list we're hovering over,
+      // then set as active
       for (var i = 0; i < this.links.length; i++) {
         if (this.links[i].title == e.target.innerHTML) {
           this.links[i].active = true;
           this.activeLinkIndex = i;
-        } else {
-          this.links[i].active = false;
         }
       }
     },
-    mouseout: function() {
-      this.active = false;
-      this.activeLink = '';
-      for (var i = 0; i < this.links.length; i++) {
-        this.links[i].active = false;
-      }
-      // this.activeLinkIndex = NaN;
-    },
-    beforeEnter: function (el) {
-      // el.style.left = 0;
-      // ...
-    },
-    // the done callback is optional when
-    // used in combination with CSS
     enter: function (el, done) {
-      // ...
-      console.log("Entering");
-      // let mouseX = e.clientX;
-      // let barX = this.$refs.bar.getBoundingClientRect().left;
-      // let minDistance = 1;
-      // let distance = Math.round(barX - mouseX);
-      // console.log(distance);
-      // if (distance > minDistance) {
-      //   this.barLeft -= 10%;
-      // } else if (distance < -minDistance) {
-      //   this.barLeft += 10%;
-      // }
+      // on entering the transition, left the left position of the bar
+      // and queue its animation (use queue to prevent stutter)
       if (this.activeLinkIndex != NaN) {
         let leftPos = this.activeLinkIndex * 25;
-        Velocity(el, { left: leftPos + '%' }, { complete: done })
-      }
-    },
-    afterEnter: function (el) {
-      // ...
-    },
-    enterCancelled: function (el) {
-      // ...
-    },
-    // --------
-    // LEAVING
-    // --------
-    beforeLeave: function (el) {
-      // ...
-    },
-    // the done callback is optional when
-    // used in combination with CSS
-    leave: function (el, done) {
-      // ...
-      console.log("Leaving");
-      // Velocity(el, { left: 0 }, { complete: done })
-    },
-    afterLeave: function (el) {
-      // ...
-    },
-    // leaveCancelled only available with v-show
-    leaveCancelled: function (el) {
-      // ...
+        Velocity(el, { left: leftPos + '%' }, { queue: this.animQueue, complete:done });
+        Velocity.Utilities.dequeue(el, this.animQueue);
+      };
     }
   }
 }
@@ -124,7 +65,11 @@ export default {
 <!-- Add "lang" attribute to use scss instead -->
 <style scoped lang="scss">
 
+$brand-primary: #0A9EE7;
+$nav-color: #7C7C7C;
+
 .navbar {
+    display: block;
     position: relative;
     max-width: 400px;
     margin: 1em auto;
@@ -133,37 +78,36 @@ export default {
 ul {
   list-style-type: none;
   padding: 0;
-  display: flex;
-  justify-content:space-around;
 }
 
 li {
+  display: inline-block;
+  width: 25%;
+  text-align: left;
+  padding: 0;
+  margin: 0;
+
   a {
-    padding: 10px;
+    display: block;
     text-decoration: none;
+    color: $nav-color;
+    transition: all .4s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+    padding: 20px 0;
+
+    &:hover {
+      color: $brand-primary;
+    }
+
   }
 }
 
 #bar {
   width: 20px;
-  height: 4px;
-  background: blue;
+  height: 3px;
+  background: $brand-primary;
   position: absolute;
   bottom: 0;
   left: 0;
 }
-
-// /* Enter and leave animations can use different */
-// /* durations and timing functions.              */
-// .bar-slide-enter-active {
-//   transition: all .3s ease;
-// }
-// .bar-slide-leave-active {
-//   transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-// }
-// .bar-slide-enter, .bar-slide-leave-to {
-//   transform: translateX(10px);
-//   opacity: 0;
-// }
 
 </style>
