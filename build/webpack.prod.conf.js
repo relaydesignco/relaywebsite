603d4e9b8bd25de0deae9ee4051b070d632d0ce7
Drew McChesney <drew@jetastudio.com>
2017-05-23 21:14:29 +0000

    Fixes issue where projects page doesn't prerender.

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -95,13 +95,14 @@ var webpackConfig = merge(baseWebpackConfig, {
     // prerender pages for SEO
     new PrerenderSpaPlugin(
       path.join(__dirname, '../dist'), // Path to compiled app
-      [ '/', 
-        '/company', 
-        '/services', 
-        '/project/garage-bar', 
-        '/project/waterfront-botanical-gardens', 
-        '/project/21c', 
-        '/project/psa', 
+      [ '/',
+        '/company',
+        '/services',
+        '/projects',
+        '/project/garage-bar',
+        '/project/waterfront-botanical-gardens',
+        '/project/21c',
+        '/project/psa',
         '/contact' ] // List of endpoints you wish to prerender
     )
   ]
