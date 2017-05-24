e86d9fefedb5e930fccdd3034e2eee3815dea8ed
ashleyrevlett <ashleyrevlett@gmail.com>
2017-05-23 21:42:55 -0400

    Adds contact page content

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,15 +2,29 @@
   <div class="contact">
 
         <header class="wrapper">
-            contact! {{msg}}
+            <h2 class="page_title">{{page_title}}</h2>
         </header>
 
         <article class="wrapper">
-          <header class="fadein">
-              <h2>Generic headline</h2>
-          </header>
-          <section class="fadein">
-              <p>Page content</p>
+          <section class="slat">
+              <div class="column-half">
+                <h2>Get in touch.</h2>
+                <p><strong>Tel</strong> 
+                <a href="tel:5023377309">502 337 7309</a><br />
+                <strong>Email</strong> 
+                <a href="mailto:hello@relaydesign.co">hello@relaydesign.co</a><br />
+                <strong>Twitter</strong> <a href="https://www.twitter.com/relaydesignco" target="_blank">@relaydesignco</a></p>
+
+                <p><strong>Mailing Address</strong><br />
+                607 West Main Street, #100<br />
+                Louisville, Kentucky 40202</p>
+                
+                
+              </div>
+              <div class="column-half">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3132.952917922801!2d-85.762332684709!3d38.25739799201804!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886972a3bfcc2ae3%3A0x6ef9a61385b0c880!2s607+W+Main+St%2C+Louisville%2C+KY+40202!5e0!3m2!1sen!2sus!4v1495589809080" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+              </div>
+
           </section>
       </article>
 
