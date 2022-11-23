8cda720469be0e87376d9b9bbc0478b8452e7eaf
ashley revlett <ashleyrevlett@gmail.com>
2022-11-23 14:02:37 -0500

    Mobile style fixes

--- src/components/WorkListItem.vue
+++ src/components/WorkListItem.vue
@@ -26,36 +26,19 @@ watchEffect(() => {
 </script>
 
 <template>
-  <div class="relative mb-12 md:h-96" :class="{ isVisible: isVisible, notVisible: !isVisible}">
-    <router-link :to="routeUrl" class="md:h-96 flex flex-col md:flex-row items-center justify-end mb-8 title">
+  <div class="relative mb-12 md:h-80 lg:h-96" :class="{ isVisible: isVisible, notVisible: !isVisible}">
+    <router-link :to="routeUrl" class="md:h-80 lg:h-96 flex flex-col md:flex-row items-center justify-end mb-8 title">
       <div class="relative z-10 md:w-1/2 order-2 md:order-1">
         <h2>{{ title }}</h2>
-        <p class="text-black description medium inline-block w-auto mb-12 md:mb-0 ml-4">{{ description }}</p>
+        <p class="text-black description medium !leading-tight inline-block w-auto mb-12 md:mb-0 md:ml-4">{{ description }}</p>
       </div>
-      <img :src="imgSrc" alt="" class="content-img md:absolute md:left-2 order-1 md:order-2 z-0 md:h-96 mb-2 md:w-1/2 object-cover" />
+      <img :src="imgSrc" alt="" class="content-img md:absolute md:left-2 order-1 md:order-2 z-0 md:h-80 lg:h-96 mb-2 md:w-1/2 object-cover" />
     </router-link>
     <div ref="visibleMarker" class="visibleMarker"></div>
   </div>
 </template>
 
 <style scoped>
-
-h2 {
-  line-height: 1;
-  color: #0000;
-  background:
-    linear-gradient(90deg, v-bind(clipColor) 50%,v-bind(textColor) 0)
-    var(--_p,100%)/200% no-repeat;
-  -webkit-background-clip: text;
-          background-clip: text;
-  transition: .15s ease-in;
-  margin-left: -120px;
-  margin-right: 60px;
-}
-
-a:hover h2 {
-  --_p: calc(100% - 112px * -1);
-}
 
 .visibleMarker {
   position: absolute;
@@ -65,9 +48,28 @@ a:hover h2 {
   /* background: blue; */
 }
 
+h2 {
+  @apply text-blue leading-none;
+
+  font-size: 40px;
+}
+
 @screen md {
   h2 {
     font-size: 60px;
+    color: #0000;
+    background:
+      linear-gradient(90deg, v-bind(clipColor) 50%,v-bind(textColor) 0)
+      var(--_p,100%)/200% no-repeat;
+    -webkit-background-clip: text;
+            background-clip: text;
+    transition: .15s ease-in;
+    margin-left: -120px;
+    margin-right: 60px;
+  }
+
+  a:hover h2 {
+    --_p: calc(100% - 112px * -1);
   }
 }
 
@@ -84,24 +86,26 @@ a:hover h2 {
     opacity: 0;
     transition: opacity .25s ease;
   }
+
+  a:hover .description {
+    opacity: 1;
+  }
 }
 
- a:hover .description {
-  opacity: 1;
-}
+@screen md {
+  .content-img  {
+    transition: opacity .5s ease, transform .5s ease;
+    transform-origin: center right;
+    opacity: 0;
+  }
 
-.content-img  {
-  transition: opacity .5s ease, transform .5s ease;
-  transform-origin: center right;
-  opacity: 0;
-}
+  a:hover .content-img {
+    opacity: 1;
+  }
 
-a:hover .content-img {
-  opacity: 1;
-}
-
-a:hover .content-img {
-  transform: translate(-1rem, -.4rem);
+  a:hover .content-img {
+    transform: translate(-1rem, -.4rem);
+  }
 }
 
 </style>
