bfe1d9d2aaac3c2fa4aed1d01db775995d4752fc
ashley revlett <ashleyrevlett@gmail.com>
2022-11-30 12:12:51 -0500

    Make work page type-only layout

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import WorkListItem from '../components/WorkListItem.vue'
+import WorkListItemType from '../components/WorkListItemType.vue'
 import Footer from '../components/Footer.vue'
 </script>
 
@@ -10,12 +10,12 @@ import Footer from '../components/Footer.vue'
       <div class="flex flex-col px-8 md:px-24 items-center justify-center">
         <h1 class="h1 text-center text-blue mb-40 mt-56">Work.</h1>
       </div>
-      <div class="px-8 md:px-32 py-12 pb-48">
-          <WorkListItem clipColor="#F37062" textColor="#ffffff" title="American Airlines" description="An online home for a global workforce" img-src="/fpo.png" route-url="/work/american-airlines" />
-          <WorkListItem clipColor="#F37062" textColor="#ffffff" title="GE Appliances" description="Virtual experience as sales tool" img-src="/fpo.png" route-url="/work/american-airlines" />
-          <WorkListItem clipColor="#F37062" textColor="#ffffff" title="Embodied Labs" description="Immersive online training platform" img-src="/fpo.png" route-url="/work/american-airlines" />
-          <WorkListItem clipColor="#F37062" textColor="#ffffff" title="21c Museum Hotels" description="Business intelligence tools for hospitality at scale" img-src="/fpo.png" route-url="/work/american-airlines" />
-          <WorkListItem clipColor="#F37062" textColor="#ffffff" title="Odessa Connect" description="Multi-platform app for senior living residences" img-src="/fpo.png" route-url="/work/american-airlines" />
+      <div class="px-4 md:px-32 py-12 pb-36">
+          <WorkListItemType title="American Airlines" img-src="/hero-aa.jpg" route-url="/work/american-airlines" />
+          <WorkListItemType title="GE Appliances" img-src="/hero-gea.jpg" route-url="/work/american-airlines" />
+          <WorkListItemType title="Embodied Labs" img-src="/hero-el.jpg" route-url="/work/american-airlines" />
+          <WorkListItemType title="21c Museum Hotels" img-src="/hero-aa.jpg" route-url="/work/american-airlines" />
+          <WorkListItemType title="OdessaConnect" img-src="/hero-gea.jpg" route-url="/work/american-airlines" />
         </div>
     </section>
 
