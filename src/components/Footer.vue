a9864aa2343b0751a3587550e4904f419d3c0ed9
ashley revlett <ashleyrevlett@gmail.com>
2023-02-10 15:13:03 -0500

    Fix social links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,8 +11,8 @@
       <router-link class="bar-link text-white leading-loose mr-5 text-base mb-2" to="/work"><span>Work.</span></router-link>
       <router-link class="bar-link text-white leading-loose mr-5 text-base mb-2" to="/approach"><span>Approach.</span></router-link>
       <router-link class="bar-link text-white leading-loose mr-5 text-base mb-2" to="/about"><span>About.</span></router-link>
-      <a href="https://www.instagram.com/relaydesignco" target="_blank" class="bar-link font-sans text-white text-base leading-loose md:ml-auto mr-5 mb-2"><span>LinkedIn.</span></a>
-      <a href="https://www.linkedin.com/company/relaydesignco" target="_blank" class="bar-link font-sans text-white text-base leading-loose mb-2"><span>Instagram.</span></a>
+      <a href="https://www.linkedin.com/company/relaydesignco" target="_blank" class="bar-link font-sans text-white text-base leading-loose md:ml-auto mr-5 mb-2"><span>LinkedIn.</span></a>
+      <a href="https://www.instagram.com/relaydesignco" target="_blank" class="bar-link font-sans text-white text-base leading-loose mb-2"><span>Instagram.</span></a>
     </div>
   </section>
 </template>
