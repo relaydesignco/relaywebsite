2ede2c43316c2e08f41cf88d595bb8dfa40f7b2f
ashley revlett <ashleyrevlett@gmail.com>
2023-04-04 11:59:55 -0400

    Add office location to contact

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -27,10 +27,13 @@ useSeoMeta({
             <h3 class="text-lg md:text-2xl font-bold font-sans leading-tight mb-2">New Business</h3>
             <p><a href="mailto:hello@relaydesign.co" class="text-red font-sans font-bold text-lg md:text-2xl">hello@relaydesign.co</a></p>
             <h3 class="text-lg md:text-2xl font-bold font-sans leading-tight mt-12  mb-2">Careers</h3>
-            <p class="mb-20"><a href="mailto:careers@relaydesign.co" class="text-red font-sans font-bold text-lg md:text-2xl">careers@relaydesign.co</a></p>
+            <p class="mb-2"><a href="mailto:careers@relaydesign.co" class="text-red font-sans font-bold text-lg md:text-2xl">careers@relaydesign.co</a></p>
+            <h3 class="text-lg md:text-2xl font-bold font-sans leading-tight mt-12  mb-2">Office</h3>
+            <p class="mb-2"><a href="https://www.google.com/maps/place/Relay+Design+Co./@38.257386,-85.760121,15z/data=!4m2!3m1!1s0x0:0xa981de6cfa0800fe?sa=X&ved=2ahUKEwjXyMmyyJD-AhW7CjQIHdXPB-EQ_BJ6BAgzEAk" target="_blank" class="text-red font-sans font-bold text-lg md:text-2xl">607 West Main Street<br />Louisville, KY 40202</a></p>
           </div>
           <div class="lg:w-1/4">
-            <p class="mt-2 mb-4 leading-loose"><a href="https://www.linkedin.com/company/relaydesignco" target="_blank" class="text-red font-sans font-bold text-lg">LinkedIn.</a></p>
+            <p class=" text-lg font-bold font-sans leading-tightloose mt-3 mb-3">Follow Us</p>
+            <p class="mt-2 mb-2 leading-loose"><a href="https://www.linkedin.com/company/relaydesignco" target="_blank" class="text-red font-sans font-bold text-lg">LinkedIn.</a></p>
             <p><a href="https://www.instagram.com/relaydesignco" target="_blank" class="text-red font-sans font-bold text-lg">Instagram.</a></p>
           </div>
         </div>
